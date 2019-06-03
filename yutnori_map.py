f400c1d73a1d3246557d144724e316d9f1c97331
SYseon <43339426+SYseon@users.noreply.github.com>
2019-06-04 00:19:37 +0900

    백도, 원점 넘어가면 들어감, 기타 오류 수정
    테스트 필요

--- yutnori_map.py
+++ yutnori_map.py
@@ -6,8 +6,6 @@ This is a temporary script file.
 """
 
 
-
-
 class Map():
     """
     gksrmfdl dkscuwla.
@@ -48,9 +46,9 @@ class Map():
             the number of pieces in each node.
             
         """
-        self.map_list = [[False, None, -1, 0], [False, None, -1, 0], [False, None, -1, 0], [False, None, -1, 0], [True, None, -1, 0],
-                          [False, 'up', -1, 0], [False, None, -1, 0], [False, None, -1, 0], [False, None, -1, 0], [True, None, -1, 0],
-                          [False, 'down', -1, 0], [False, None, -1, 0], [False, None, -1, 0], [False, None, -1, 0], [None, None, -1, 0],
+        self.map_list = [[False, None, -1, 0], [False, None, -1, 0], [False, None, -1, 0], [False, None, -1, 0], [False, None, -1, 0],
+                          [True, 'up', -1, 0], [False, None, -1, 0], [False, None, -1, 0], [False, None, -1, 0], [False, None, -1, 0],
+                          [True, 'down', -1, 0], [False, None, -1, 0], [False, None, -1, 0], [False, None, -1, 0], [None, None, -1, 0],
                           [False, None, -1, 0], [False, None, -1, 0], [False, None, -1, 0], [False, None, -1, 0], [None, None, -1, 0],
                           [False, 'up', -1, 0], [False, 'up', -1, 0], [False, 'up', -1, 0], [False, 'up', -1, 0],
                           [False, 'down', -1, 0], [False, 'down', -1, 0], [False, 'down', -1, 0], [False, 'down', -1, 0],
@@ -72,7 +70,6 @@ class Map():
         """
         self.user_info = user_info
         self.n_roll = n_roll
-        
         if map_index == 0 and self.user_remained[user_info] > 0:
             self.map_list[map_index][2] = user_info
             self.map_list[map_index][3] = 1
@@ -81,21 +78,35 @@ class Map():
         if self.map_list[map_index][2] != user_info:
             raise ValueError("YOU SELECTED A WRONG NODE")
         
-        if map_index == len(self.map_list) - 1:
-            if self.map_list[map_index][1] == 'up':
-                if n_roll <= 2:
-                    self.change_info(map_index, 21 + n_roll)
+        if self.n_roll == -1:
+            if map_index == 28:
+                if self.map_list[map_index][1] == 'up':
+                    self.change_info(map_index, 21)
                 else:
-                    self.change_info(map_index, 12 + n_roll)
+                    self.change_info(map_index, 25)
+            elif map_index == 22 or map_index == 26:
+                self.change_info(map_index, 28)
+            elif map_index == 24:
+                self.change_info(map_index, 10)
+            elif map_index == 20:
+                self.change_info(map_index, 5)
+            elif map_index == 15:
+                if self.map_list[map_index][1] =='up':
+                    self.change_info(map_index, 23)
+                else:
+                    self.change_info(map_index, 14)
+            else:
+                self.change_info(map_index, map_index -1)
             
-            elif self.map_list[map_index][1] == 'down':
-                if n_roll <= 2:
-                    self.change_info(map_index, 25 + n_roll)
-                else:
-                    self.change_info(map_index, (17 + n_roll)%20)
+            
+        elif map_index == len(self.map_list) - 1:
+            if n_roll < 3:
+                self.change_info(map_index, 25 + n_roll)
+            else:
+                self.change_info(map_index, 0)
                     
         elif not self.map_list[map_index][0] and self.map_list[map_index][1] == None:
-            self.change_info(map_index, (map_index + n_roll)%20)
+            self.change_info(map_index, map_index + n_roll)
         
         elif self.map_list[map_index][0] == True:
             if self.map_list[map_index][1] == 'up':
@@ -140,7 +151,7 @@ class Map():
                 else:
                     self.change_info(map_index, map_index + n_roll - 9)
                     
-        elif self.map_list[map_index][1] == 'up':
+        elif self.map_list[map_index][1] == 'down':
             if map_index == 24 or map_index ==25:
                 if map_index + n_roll == 26:
                     self.change_info(map_index, len(self.map_list) - 1)
@@ -158,24 +169,32 @@ class Map():
                     self.change_info(map_index, map_index + n_roll - 28)
               
         
-        return self.map_list, self.winner, self.user_info
+        return self.map_list, self.winner, self.user_info, self.in_goal
         
     def change_info(self, departure, destination):
+        turn_change = True
         
-        if destination == 0:
-            self.in_goal[self.user_info] = self.in_goal[self.user_info] + self.map_list[departure][3]
-            
-            
-        elif destination == 28:
+        
+        if destination == 28:
+            self.map_list[destination][1] = self.map_list[departure][1]
+        
+        elif destination == 15:
             self.map_list[destination][1] = self.map_list[departure][1]
             
-        if self.map_list[destination][2] == self.map_list[departure][2]:
+        if (destination < 5 and departure > 15) or ( self.n_roll == -1 and destination == 0):
+            self.in_goal[self.user_info] = self.in_goal[self.user_info] + self.map_list[departure][3]
+            self.map_list[departure][2] = -1
+            self.map_list[departure][3] = 0
+          
+        elif self.map_list[destination][2] == self.map_list[departure][2]:
                 self.map_list[destination][3] = self.map_list[destination][3] + self.map_list[departure][3]
                 self.map_list[destination][2] = self.map_list[departure][2]
                 self.map_list[departure][2] = -1
                 self.map_list[departure][3] = 0
                 
         else:
+                if self.map_list[destination][2] != -1:
+                    turn_change = False
                 self.map_list[destination][2] == self.map_list[departure][2]
                 self.map_list[destination][3] = self.map_list[departure][3]
                 self.map_list[destination][2] = self.map_list[departure][2]
@@ -184,24 +203,14 @@ class Map():
         
         if self.in_goal[self.user_info] == 4:
                 self.winner = self.user_info
-    
-        if self.n_roll < 4:
+        
+        if self.n_roll < 4 and turn_change:
             self.user_info = (self.user_info + 1)%4
             
 
 
 
 ad = Map()
-print(ad.select(5, 0, 0))
-
-
-
-
-
-
-
-
-
 
 
 
